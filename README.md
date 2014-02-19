894fcea2282faac1a86bcb886ff0cd61f75f8ccb
Phil Birnie <pbirnie@marketingadept.com>
2014-02-18 23:41:48 -0500

    Update to version 0.9; major fixes to plugin structure.

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 Equal Height Plugin
-===========
+====================
 
 Author: Phil Birnie, Adept Marketing
-Version: 0.2
+Version: 0.9
+
 
 jQuery plugin that sets a collection of items to equal height.
+
+## Options
+
+* extraSpacing: 0 - creates extra padding at the bottom of the element beyond computed height.
+
+## Methods
+
+* `resizeElements`: This can be called at anytime and will automatically reset and recalculate the size of your elements.  Very useful for responsive design.
+
+## How to use
+
+`var resizer = new $.equalHeight($(elementCollection));`
+
+If your elements contain images, you should fire this on the `window.load` event or your calculated height may be too small. 
+
+For responsive sites, you may need to recalculate the height when the window changes size: 
+
+    var resizer = new $.equalHeight($("#featured-products article"));
+    $(window).bind("resize", function() {
+        resizer.resizeElements();
+    });
+    
+
+_Last Updated: February 18, 2014_
--- equalHeight.js
+++ equalHeight.js
@@ -4,22 +4,77 @@
 
 (function($) {
     "use strict";
-    $.fn.equalHeight = function() {
-        var maxHeight, elementHeight;
-        maxHeight = 0;
-        elementHeight = 0;
+    $.equalHeight = function(el, options) {
+        var defaults, plugin, init, resetHeight, setCollectionHeight;
 
-        $(this).each(function() {
-            elementHeight = $(this).height();
-            if (elementHeight > maxHeight) {
-                maxHeight = elementHeight;
-            }
-        });
-        //Now set all callouts to max height as long as they were found...
-        if (maxHeight !== 0) {
-            $(this).each(function() {
-                $(this).css("height", maxHeight + "px");
+        defaults = {
+            extraSpacing: 0
+        };
+
+        plugin = this;
+
+        plugin.settings = {};
+
+        init = function() {
+            plugin.settings = $.extend({}, defaults, options);
+            plugin.el = el;
+            plugin.resizeElements();
+        };
+
+
+        /**
+         * Resets height of elements so that they can be
+         * accurately resized
+         * @return {void}
+         */
+        resetHeight = function() {
+            $(plugin.el).each(function() {
+                $(this).css("height", "auto");
             });
-        }
+        };
+
+        /**
+         * Sets the height of collection elements
+         * @param {int} height height to which elements should be set
+         */
+        setCollectionHeight = function(height) {
+            $(plugin.el).each(function() {
+                $(this).css("height", height + "px");
+            });
+        };
+
+        /**
+         * Public function to resize Elements at will
+         * @return {void}
+         */
+        plugin.resizeElements = function() {
+            var maxHeight, elementHeight;
+
+            maxHeight = 0;
+            elementHeight = 0;
+
+            /**
+             * Reset Height of all elements
+             */
+            resetHeight();
+
+            $(plugin.el).each(function() {
+                elementHeight = parseInt($(this).outerHeight(), 10) + plugin.settings.extraSpacing;
+                if (elementHeight > maxHeight) {
+                    maxHeight = elementHeight;
+                }
+            });
+
+            //Now set all callouts to max height as long as they were found.
+            if (maxHeight !== 0) {
+                setCollectionHeight(maxHeight);
+            }
+        };
+
+        /**
+         * Initialize Plugin
+         **/
+        init();
+
     };
 }(jQuery));
